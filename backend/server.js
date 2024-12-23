a592dcb0e8719862c8d4dbb3840c0264b9886f46
Lionel <lpei1900@gmail.com>
2024-12-22 19:17:09 -0500

    Implemented sign up user controller

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,13 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 5000;
+dotenv.config();
 
-app.get('/', (req,res)=> {
-  res.send("hello");
-});
-
-
-
+app.use(express.json());
 app.use("/api/auth", authRoutes);
+
+
 
 app.listen(PORT, ()=> {
   connectToMongoDB();
