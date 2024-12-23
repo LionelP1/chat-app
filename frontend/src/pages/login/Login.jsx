170c9146f2dd8e4a7090bff2e82e7cb08ca04df7
Lionel <lpei1900@gmail.com>
2024-12-23 13:50:13 -0500

    Created a bit of login component with glass effect

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+        <h1 className='text-3xl font-semibold text-center text-gray-300'>
+					Login
+					<span className='text-blue-500'>ChatApp</span>
+				</h1>
+      </div>
+
+    </div>
   )
 }
 
-export default Login
+export default Login;
