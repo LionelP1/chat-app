3630d33baec3b511e5180a0040886154eb5e2f1e
Lionel <lpei1900@gmail.com>
2024-12-22 23:38:21 -0500

    Created message routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -9,7 +10,9 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 
