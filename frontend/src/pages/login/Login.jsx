731a515e4a3c006838bf9aedf62b2b623cd8ef21
Lionel <lpei1900@gmail.com>
2024-12-24 19:28:27 -0500

    Updated the login page

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
 
-function Login() {
+const Login = () => {
 
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
+
+	const { loading, login } = useLogin();
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		await login(username, password);
 	};
-	
+
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -51,12 +55,10 @@ function Login() {
 					</Link>
 
           <div>
-						<button className='btn btn-block btn-sm mt-2'>Login
+						<button className='btn btn-block btn-sm mt-2' disabled={loading}>
+							{loading ? <span className='loading loading-spinner '></span> : "Login"}
 						</button>
 					</div>
-
-
-
 				</form>
       </div>
 
