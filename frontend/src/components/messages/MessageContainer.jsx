3d02be3a173e1ec6fb466797699a7327d3c7b2e3
Lionel <lpei1900@gmail.com>
2024-12-25 10:41:42 -0500

    Updated Background image, updated message MessageContainer

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -2,9 +2,15 @@ import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
 import useConversation from "../../zustand/useConversation";
+import { useEffect } from "react";
 
 const MessageContainer = () => {
 	const { selectedConversation, setSelectedConversation } = useConversation();
+
+	useEffect(() => {
+		return () => setSelectedConversation(null);
+	}, [setSelectedConversation]);
+
 	return (
 		<div className='md:min-w-[450px] flex flex-col'>
 				{!selectedConversation ? (
