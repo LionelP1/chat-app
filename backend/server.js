7582b2d0b422c2b254321af56e072ee0a7a97ea2
Lionel <lpei1900@gmail.com>
2024-12-26 11:44:09 -0500

    Set up socket in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app } from "./socket/socket.js"
 
 const PORT = process.env.PORT || 5000;
 dotenv.config();
@@ -18,10 +19,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-
-
-
-app.listen(PORT, ()=> {
+server.listen(PORT, ()=> {
   connectToMongoDB();
   console.log(`Server running on port ${PORT}`);
 
